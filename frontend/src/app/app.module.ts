4b9364ca346418313ff06f997ddb61ea3547149a
thay-leth <yhbae4@gmail.com>
2023-12-01 21:34:06 -0500

    Created login and signup pages
    Continue Button is not in the correct place

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    SignupPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
